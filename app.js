e733e6abafeb9b75b3d1a193253897da23bb8e76
Cryptokitana <yelenakensborn@gmail.com>
2024-02-12 22:06:59 +0100

    added sepholia endpoint

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const displayBalance = document.querySelector('#balance');
 const sendTrxButton = document.querySelector('#sendTrx');
 const amountInput = document.querySelector('#amount');
 const toAcoountInput = document.querySelector('#toAccount')
-const provider = new ethers.providers.JsonRpcProvider('HTTP://127.0.0.1:7545');
+const provider = new ethers.providers.JsonRpcProvider('https://sepolia.infura.io/v3/9963e6e3427443cbbee6173405b34190');
 //const provider = new ethers.providers.JsonRpcProvider('HTTP://127.0.0.1:7545');
 
 let account;
