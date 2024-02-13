6e525f28ace3696f24abdee57dca3ac88d4a446e
Cryptokitana <yelenakensborn@gmail.com>
2024-02-13 21:24:42 +0100

    added history

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const displayBalance = document.querySelector('#balance');
 const sendTrxButton = document.querySelector('#sendTrx');
 const amountInput = document.querySelector('#amount');
 const toAcoountInput = document.querySelector('#toAccount')
-const provider = new ethers.providers.JsonRpcProvider('https://sepolia.infura.io/v3/9963e6e3427443cbbee6173405b34190');
+const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/9963e6e3427443cbbee6173405b34190');
+//const provider = new ethers.providers.JsonRpcProvider('https://sepolia.infura.io/v3/9963e6e3427443cbbee6173405b34190');
 //const provider = new ethers.providers.JsonRpcProvider('HTTP://127.0.0.1:7545');
 
 let account;
@@ -16,9 +17,20 @@ function initApp(){
     console.log(ethers);
 }
 async function checkBalance(){
-    account = accountInput.value;
+    let scanProvider = new ethers.providers.EtherscanProvider();
+    //account = accountInput.value;
     const balance = await provider.getBalance(account);
+    //console.log(await provider.getTransactionCount(account));
+    const history = await scanProvider.getHistory(account);
+    displayHistory(history);
     displayBalance.innerHTML = ethers.utils.formatEther(balance);
+}
+
+async function displayHistory(transactions){
+    for(let trx of transactions){
+        console.log(trx);
+        console.log(trx.blockNumber);
+    }
 }
 
 async function sendTransaction(){
